440253b9d27d86fe559afb34e464598e058ca763
KaraniAbdellah <abdellahkarani@gmail.com>
2025-12-31 19:30:36 +0100

    feat: add UI and Logic Folder

---
+++ all-in-one/src/Logic/x2j_j2x/Xml_To_Json.java
@@ -0,0 +1,23 @@
+package Logic.x2j_j2x;
+
+public class Xml_To_Json {
+    public String ToXml(String data) {
+        String xmlData = "THIS XML DATA";
+        return xmlData;
+    }
+
+    public String ToJson(String data) {
+        String jsonData = "THIS XML DATA";
+        return jsonData;
+    }
+
+    public String ToXmlWithApi(String data) {
+        String xmlData = "THIS XML DATA";
+        return xmlData;
+    }
+
+    public String ToJsonWithApi(String data) {
+        String jsonData = "THIS XML DATA";
+        return jsonData;
+    }
+}
